22aa2e905cbbc6cb76cbf904da9ffff0e97289bc
LogangBrown3 <26lbrown@standish-sterling.org>
2024-10-03 13:02:24 -0400

    1HourMiniChallenge

---
+++ OneHourMiniChallenge.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class OneHourMiniChallenge
+{
+    public static void main(String[] args)
+    {
+        int numberx;
+        int numbery;
+        int sum;
+        int difference;
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.println("Enter a number.");
+        numberx = Keyboard.nextInt();
+        System.out.println("Enter another number.");
+        numbery = Keyboard.nextInt();
+        Keyboard.close();
+        sum = numberx + numbery;
+        difference = numberx - numbery;
+        System.out.println("Addition result: " + sum);
+        System.out.println("Subtraction result: " + difference);
+        
+
+
+
+
+
+    }
+}
+ 
+
+    
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+
